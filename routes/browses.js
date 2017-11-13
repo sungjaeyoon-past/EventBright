84dbf241ec195722d15fc2ce799f7054ed5aa1e6
codingHack <codingHack>
2017-11-14 00:41:39 +0900

    add browse schema

--- routes/browses.js
+++ routes/browses.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const Browse = require('../models/browse')//스키마 정의
+
+
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('browses/browse');
-  });
+   res.render('browses/browse');
+
+});
   
   
-  module.exports = router;
+ module.exports = router;
