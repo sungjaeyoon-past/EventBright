c5e7b125b4e6107fc3302504023ee52e0728a44f
codingHack <codingHack>
2017-12-01 03:16:30 +0900

    add recommend by reviewnum,parnum,readnum

--- routes/recommend.js
+++ routes/recommend.js
@@ -1,12 +1,23 @@
 var express = require('express');
 const User = require('../models/user');
+const Question = require('../models/question');
+const Review = require('../models/review');
 var router = express.Router();
 const catchErrors = require('../lib/async-error');
 
 
-router.get('/', catchErrors(async (req, res, next) => {
+router.get('/calender', catchErrors(async (req, res, next) => {
   const users = await User.find({});
-  res.render('recommend/index');
+  res.render('recommend/calender');
+}));
+
+router.get('/interest', catchErrors(async (req, res, next) => {
+  const users = await User.find({});
+  const questionParticipateMax = await Question.find().sort({numParticipate:-1}).limit(5);
+  const questionReviewMax = await Question.find().sort({numReviews:-1}).limit(5);
+  const questionReadMax = await Question.find().sort({numReads:-1}).limit(5);
+  res.render('recommend/index',{questionParticipateMax:questionParticipateMax ,
+    questionReviewMax:questionReviewMax, questionReadMax:questionReadMax });
 }));
 
 
